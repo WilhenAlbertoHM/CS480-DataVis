99df172cea7a6b016418155c2363211196d6ded9
Wilhen Alberto Hui Mei <wahm0131@gmail.com>
2024-02-11 16:22:05 -0500

    Finish HW2

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     const tooltips = document.querySelectorAll(".tooltip");
-    const infographic = document.getElementsByClassName(".infographic-containter");
+    const elementsToBrighten = document.querySelectorAll(".infographic-container > *:not(.tooltip)");
+
     tooltips.forEach(tooltip => {
         tooltip.addEventListener("mouseenter", function () {
             this.style.transform = "scale(1.02)";
             this.style.opacity = "1";
-            infographic.opacity = "0.5";
+            this.style.filter = "brightness(100%)"; 
+            elementsToBrighten.forEach(element => {
+                element.style.filter = "brightness(80%)";
+            });
         });
 
         tooltip.addEventListener("mouseleave", function () {
             this.style.transform = "scale(1)";
             this.style.opacity = "0";
+            this.style.filter = "brightness(80%)";
+            elementsToBrighten.forEach(element => {
+                element.style.filter = "brightness(100%)";
+            });
         });
     });
 });
